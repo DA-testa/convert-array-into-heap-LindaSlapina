9eac36213948c639116d06b9bf7fb18a6dc81d56
LindaSlapina <125659827+LindaSlapina@users.noreply.github.com>
2023-03-19 21:14:14 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,47 @@
 # python3
+from math import floor
 
+def s_down(data, n,i,swaps):
+    x = (i << 1 ) + 1
+    y = (i << 1 ) + 2
+    index = i
+    if y<n and data[x]<data[index]:
+        index = x
+    if y<n and data[y]<data[index]:
+        index = y
+    if index !=i:
+        swaps.append((i,index))
+        data[i],data[index] = data[index], data[i]
+        s_down(data,n,index,swaps)        
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    n=len(data)
+    for i in range (n // 2 -1, -1, -1):
+        s_down(data,n,i,swaps)
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    try:
+        line = input().split()
+        if "I" in line:
+            n= int(input())
+            data = list(map(int, input().split()))
+        elif "F" in line:
+            nameofile = input()
+            with open ("tests/" + nameofile, 'r') as filee:
+                n = int(filee.readline())
+                data = list(map(int, filee.readline().split()))
+        else:
+            raise ValueError("Invalid input, please input F or I!")
+        if n != len(data):
+            raise ValueError("Invalid input, lenght of data does not match!")
+        swaps = build_heap(data)
+        print(swp[0],swp[1])
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    except ValueError:
+        print("Error")
 
 if __name__ == "__main__":
     main()
