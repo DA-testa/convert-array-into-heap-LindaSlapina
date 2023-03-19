ec11b28b59c5397a4cfb402326cf913aef080b65
LindaSlapina <125659827+LindaSlapina@users.noreply.github.com>
2023-03-19 21:33:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,47 +1,55 @@
 # python3
 from math import floor
 
-def s_down(data, n,i,swaps):
-    x = (i << 1 ) + 1
-    y = (i << 1 ) + 2
-    index = i
-    if y<n and data[x]<data[index]:
-        index = x
-    if y<n and data[y]<data[index]:
-        index = y
-    if index !=i:
-        swaps.append((i,index))
-        data[i],data[index] = data[index], data[i]
-        s_down(data,n,index,swaps)        
+# Linda Slapiņa 221RDB214
 
-def build_heap(data):
+def heap(data, a, i, swaps):
+    gar = i*2+1
+    gar2 = i*2+2
+
+    rez = i
+    if gar < a and data[gar] < data[rez]:
+        rez = gar
+    if gar2 < a and data[gar2] < data[rez]:
+        rez = gar2
+    if rez != i:
+        swaps.append((i,rez))
+        data[i], data[rez] = data[rez], data[i]
+        heap(data, a, rez, swaps)    
+
+
+def build_heap (data):
     swaps = []
-    n=len(data)
-    for i in range (n // 2 -1, -1, -1):
-        s_down(data,n,i,swaps)
+    a = len(data)
+    for i in range(a// 2-1, -1, -1):
+        heap(data, a, i, swaps)
     return swaps
 
-
 def main():
-    try:
-        line = input().split()
-        if "I" in line:
-            n= int(input())
-            data = list(map(int, input().split()))
-        elif "F" in line:
-            nameofile = input()
-            with open ("tests/" + nameofile, 'r') as filee:
-                n = int(filee.readline())
-                data = list(map(int, filee.readline().split()))
-        else:
-            raise ValueError("Invalid input, please input F or I!")
-        if n != len(data):
-            raise ValueError("Invalid input, lenght of data does not match!")
-        swaps = build_heap(data)
-        print(swp[0],swp[1])
+    text = input()
+    
+    if "I" in text:
+        n = int(input())
+        data = list(map(int, input.split()))
 
-    except ValueError:
-        print("Error")
+    elif "F" in text:
+        filee = input()
+        with open("tests/" + filee + 'r') as faili:
+            n = int(faili.readline())
+            data = list(map(int, faili.readline().split()))
+    else:
+        print("kluuda")
+        return
 
-if __name__ == "__main__":
+    assert len(data) == n
+
+    swaps = build_heap(data)
+
+    print(len(swaps))
+    for i,j in swaps:
+        print(i, j) 
+
+
+
+if __name__ == "__name__":
     main()
