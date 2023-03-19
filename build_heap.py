b8f37c16ba15d7253e1ea0ca42c21f9beb5ef86e
LindaSlapina <125659827+LindaSlapina@users.noreply.github.com>
2023-03-19 22:20:56 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,26 +1,28 @@
 # python3
 # Linda Slapiņa 221RDB214
 
-def heap(data, a, i, swaps):
+def work1(data, a, i, swaps):
     gar = 2 * i + 1
     gar2 = 2 * i + 2
 
     rez = i
     if gar < a and data[gar] < data[rez]:
         rez = gar
+        
     if gar2 < a and data[gar2] < data[rez]:
         rez = gar2
+        
     if rez != i:
         swaps.append((i,rez))
         data[i], data[rez] = data[rez], data[i]
-        heap(data, a, rez, swaps)    
+        work1(data, a, rez, swaps)    
 
 
-def build_heap (data):
+def work2 (data):
     swaps = []
     a = len(data)
     for i in range(a// 2 -1, -1, -1):
-        heap(data, a, i, swaps)
+        work1(data, a, i, swaps)
     return swaps
 
 def main():
@@ -35,9 +37,10 @@ def main():
         with open("tests/" + filee, 'r') as faili:
             n = int(faili.readline())
             data = list(map(int, faili.readline().split()))
+            
     assert len(data) == n
 
-    swaps = build_heap(data)
+    swaps = work2(data)
 
     print(len(swaps))
     for i, j in swaps:
